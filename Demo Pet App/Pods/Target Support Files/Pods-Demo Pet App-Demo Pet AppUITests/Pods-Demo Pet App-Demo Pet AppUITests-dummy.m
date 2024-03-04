f275e8567cb2d41d0b387e00473b7082c1037503
Roibu Adrian <roibu.adrian95@gmail.com>
2024-03-04 12:56:50 +0200

    Fix podfile
    Fix podfile after project name change

--- Demo Pet App/Pods/Target Support Files/Pods-Demo Pet App-Demo Pet AppUITests/Pods-Demo Pet App-Demo Pet AppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Demo_Pet_App_Demo_Pet_AppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Demo_Pet_App_Demo_Pet_AppUITests
-@end
---
+++ Demo Pet App/Pods/Target Support Files/Pods-Pet Finder/Pods-Pet Finder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pet_Finder : NSObject
+@end
+@implementation PodsDummy_Pods_Pet_Finder
+@end
